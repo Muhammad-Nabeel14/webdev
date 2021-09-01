9219bc72f5a7887aa7310ea4583b83fe94e0141f
Muhammad-Nabeel14 <83199401+Muhammad-Nabeel14@users.noreply.github.com>
2021-09-01 09:58:37 -0700

    first

---
+++ 01_tutorial/file.js
@@ -0,0 +1,35 @@
+'use strict';
+// console.log("hello world");
+// console.log("hello world");
+// // var name= 'Hammad'
+// // console.log(name);
+
+// let name; 
+// name = 'Hammad';
+// name = 'saeed';
+// console.log(name);
+
+
+// let num = 9007199254740991;
+// num = num +10
+// console.log(2**4);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// console.log(24/6*2%6);
+// console.log(4*2%6);
+// console.log(8%6);
+
+// let num = 66e-7;
+// console.log(num); 
+// let num = 12;
+// let num2 = " kch 12 bhi";
+// num2 = parseFloat(num2);   
+// console.log(num2);
+// let minNum = 5;
+// let maxNum = 10;
+// let randomNumber =Math.floor  (Math.random() * (maxNum-minNum +1) +minNum );
+// console.log(randomNumber); 
+
+let bigNumber = 9007199254740991477655647687436785368756n;
+bigNumber = bigNumber + 2456437653465873465734657364756843n;
+console.log(bigNumber);
